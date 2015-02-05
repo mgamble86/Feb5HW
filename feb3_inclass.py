2c85e056214b959db384626fe4915d80b3afc961
Mason Gamble <mgamble@ucla.edu>
2015-02-05 06:34:37 +0000

    Add inclass function work

---
+++ feb3_inclass.py
@@ -0,0 +1,15 @@
+from __future__ import division
+
+
+def get_at_content(dna):
+    length = len(dna)
+    a_count = dna.count('A')
+    t_count = dna.count('T')
+    at_content = (a_count + t_count) / length
+    return at_content
+	
+	
+my_at_content = get_at_content("ATGCCGTAGCTAGCTACGATCGTACTCGA")	
+print(str(my_at_content))
+print(get_at_content("TAGAGCATCAGTACGATACGA"))
+print(get_at_content("atgcgcagtcagctagctacgatgcat"))
